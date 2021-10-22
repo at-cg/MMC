9959e94b974fc6c6fdd5d810a87373754ffbfd23
kushagragarwal2443 <kushagragarwal2443@gmail.com>
2021-10-23 02:43:54 +0530

    KMC3 original code added

---
+++ KMC/kmc_core/raduls_avx.cpp
@@ -0,0 +1,13 @@
+/*
+  This file is a part of KMC software distributed under GNU GPL 3 licence.
+  The homepage of the KMC project is http://sun.aei.polsl.pl/kmc
+  
+  Authors: Sebastian Deorowicz, Agnieszka Debudaj-Grabysz, Marek Kokot
+  
+  Version: 3.1.1
+  Date   : 2019-05-19
+*/
+
+#include "raduls_impl.h"
+
+// ***** EOF
